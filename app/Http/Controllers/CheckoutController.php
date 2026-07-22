b4e3992d6ee5549d10f4f3d7f3daf80931bf4dd9
heisernbergg0942 <suytongleng@gmail.com>
2026-07-22 15:38:06 +0700

    Add missing Product import in CheckoutController

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Cart;
 use App\Models\Order;
 use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Stripe\Stripe;
